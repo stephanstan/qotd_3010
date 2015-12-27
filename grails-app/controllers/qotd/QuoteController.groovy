4834314dd6590bbe594ce7016d536716889a9d39
Stephan Stan <stephanstan@gmail.com>
2015-12-26 16:50:35 -0800

    custom gsp page

--- grails-app/controllers/qotd/QuoteController.groovy
+++ grails-app/controllers/qotd/QuoteController.groovy
@@ -9,4 +9,11 @@ class QuoteController {
     def home(){
         render "<h1>Real Programmers do not eat Quiche</h1>"
     }
+
+    def random(){
+        def staticAuthor = "Anonymous"
+        def staticContent = "Real Programmers don't eat much quiche"
+        [author: staticAuthor, content: staticContent]
+
+    }
 }
